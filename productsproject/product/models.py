12c6b8741b2c109c53907b2455fc4837f30987d6
dmytrokorolchuk498-web <dmytrokorolchuk498@gmail.com>
2025-12-04 18:55:57 +0200

    I've created Product model, configured PostgreSQL in settings.py Made migration and registered site on admin panel

--- productsproject/product/models.py
+++ productsproject/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-from django.db.models.functions import Now
+from datetime import datetime
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(default=datetime.now())
+    
+    def __str__(self):
+        return f"{self.name}"
